f64c9aea34de2a2356e6e3a95b94080b838e6f1c
Benjamin Silas Rhodes <benjamin.s.rhodes@gmail.com>
2015-06-10 19:41:50 -0700

    sassy

--- Brocfile.js
+++ Brocfile.js
@@ -1,8 +1,6 @@
 /* global require, module */
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
-var pick = require('broccoli-static-compiler');
-var mergeTrees = require('broccoli-merge-trees');
 
 var app = new EmberApp({
   fingerprint: {
@@ -13,22 +11,27 @@ var app = new EmberApp({
   sourcemaps: {
     enabled: true,
   },
+
+   'ember-cli-bootstrap-sassy': {
+     'glyphicons': true,
+     'js': ['dropdown'],
+   }
 });
 
 app.import('bower_components/moment/moment.js');
 //app.import('bower_components/fastclick/lib/fastclick.js');
-app.import('bower_components/bootstrap-sass-official/assets/javascripts/bootstrap/dropdown.js');
 app.import('bower_components/d3/d3.js');
 app.import('bower_components/c3/c3.js');
 app.import('bower_components/c3/c3.css');
 
-var bootstrapFonts = pick('bower_components/bootstrap-sass-official/assets/fonts/bootstrap', {
-  srcDir: '/',
-  destDir: '/assets/boostrap'
-});
+//var bootstrapFonts = pick('bower_components/bootstrap-sass-official/assets/fonts/bootstrap', {
+  //srcDir: '/',
+  //destDir: '/assets/boostrap'
+//});
 
-module.exports = mergeTrees([
-  app.toTree(),
-  bootstrapFonts,
-]);
+module.exports = app.toTree();
+//module.exports = mergeTrees([
+  //app.toTree(),
+  //bootstrapFonts,
+//]);
 
