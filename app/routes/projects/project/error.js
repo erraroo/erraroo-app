f758aa1447397305c25fd3b82636651a4bba4919
Benjamin Silas Rhodes <benjamin.s.rhodes@gmail.com>
2015-08-29 14:33:08 -0700

    mark as seen

--- app/routes/projects/project/error.js
+++ app/routes/projects/project/error.js
@@ -2,6 +2,6 @@ import Authenticated from 'erraroo/routes/authenticated';
 
 export default Authenticated.extend({
   afterModel: function(model) {
-    model.set('wasNew', false);
+    model.set('seen', true);
   },
 });
