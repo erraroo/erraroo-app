b70030250beb30abae6f69c9588411e2e7b08aae
Benjamin Silas Rhodes <benjamin.s.rhodes@gmail.com>
2015-09-03 19:40:35 -0700

    add delete invite

--- app/routes/invitations.js
+++ app/routes/invitations.js
@@ -4,4 +4,15 @@ export default Authenticated.extend({
   model: function() {
     return this.store.findAll('invitation');
   },
+
+  actions: {
+    deleteInvitation(i) {
+      if (confirm('Are you sure you want to delete this invitation?')) {
+        const that = this;
+        i.destroyRecord().then(function() {
+          that.get('flashMessages').success('The invitation has been deleted');
+        });
+      }
+    }
+  }
 });
