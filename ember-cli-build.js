91df57b58e65609209905dbaaf3256a2faa9c02d
Benjamin Silas Rhodes <benjamin.s.rhodes@gmail.com>
2015-09-20 16:38:51 -0700

    typo

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     fingerprint: {
-      prepend: 'https://1bbicx3ding66.cloudfront.net/'
+      prepend: 'https://d1bbicx3ding66.cloudfront.net/'
     },
 
     sourcemaps: {
