3b3fc16e4ef9230a19abc108ff16bf893f80c638
Benjamin Silas Rhodes <benjamin.s.rhodes@gmail.com>
2015-10-10 15:33:58 -0700

    fix deprications

--- app/helpers/start-line.js
+++ app/helpers/start-line.js
@@ -1,21 +1,22 @@
 import Ember from 'ember';
 
+const { isNone } = Ember;
+
 export function startLine(params/*, hash*/) {
-  if (Ember.isNone(params)) {
+  if (isNone(params)) {
     return 0;
   }
 
   const frame = params[0];
-  if (Ember.isNone(frame.origLine) || frame.origLine === 0) {
+  if (isNone(frame.origLine) || frame.origLine === 0) {
     return 0;
   }
 
-  if (Ember.isNone(frame.preContext)) {
+  if (isNone(frame.preContext)) {
     return 0;
   }
-
 
   return frame.origLine - frame.preContext.length;
 }
 
-export default Ember.HTMLBars.makeBoundHelper(startLine);
+export default Ember.Helper.helper(startLine);
