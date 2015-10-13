6872feb6a50289895c94cea3f881eb7adc94a479
Benjamin Silas Rhodes <benjamin.s.rhodes@gmail.com>
2015-10-13 16:26:07 -0700

    and we have outdated depencies and stuff :)

--- app/models/outdated-revision.js
+++ app/models/outdated-revision.js
@@ -1,6 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  sha: DS.attr('string'),
   project: DS.belongsTo('project', { async: false }),
+  dependencies: DS.attr('array'),
 });
 
--- app/models/project.js
+++ app/models/project.js
@@ -9,7 +9,7 @@ export default DS.Model.extend({
   unresolvedCount: DS.attr('number'),
 
   repository: DS.belongsTo('repository', { async: true }),
-  outdatedRevisions: DS.belongsTo('outdated-revision', { async: true }),
+  outdatedRevisions: DS.hasMany('outdated-revision', { async: true }),
 
   regenerateToken() {
     const name = this.constructor.modelName;
